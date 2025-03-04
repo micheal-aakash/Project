3e1bac88587ef18bfa05ea2a6505dd1447d92fd7
unknown <cat14@LC.EDU>
2025-03-05 00:57:24 +0530

    all files are added

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([8.7832, 78.1348], 5); // Centered on Tuticorin
+var map = L.map('map').setView([8.7832, 78.1348], 5); 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
